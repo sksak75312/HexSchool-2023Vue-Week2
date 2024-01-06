985a8fbfbe3b716726037a49241b9ed41770b196
Eero <sksak75312@gmail.com>
2024-01-06 15:55:10 +0800

    Create Environment

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<!-- <script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script> -->
+
+<template>
+  <div>
+    <button type="button" class="btn btn-primary">測試</button>
+  </div>
+</template>
